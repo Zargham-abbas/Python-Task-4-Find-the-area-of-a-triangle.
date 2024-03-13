0f0ce7f6c9b10aa3c097ff569be745bdec056a3f
Zargham Abbas <zarghamabbas859@gmail.com>
2024-03-13 21:27:00 +0500

    Add files via upload

---
+++ area.py
@@ -0,0 +1,19 @@
+# Zargham-abbas
+# zarghamabbas859@gmail.com
+
+#Question No. 7:
+#Write a Python program to find the area of a triangle.
+
+#First we declare our desired variables variables to find area of triangle.
+
+base = int(input("Enter the base of the triangle :"))
+height = int(input("Enter the height of the triangle :"))
+
+# Declare the formula of area to find area.
+area = (1/2)*base*height
+
+#Now we use print function to calculate area of triangle.
+print("Area of triangle is:",area)
+
+# Run the program.
+# Done!
